5334d5ce105ddebf6ee19d675b92e89708b3d24c
Simon Prosser <simon@pagelines.com>
2014-01-09 23:42:53 +0000

    fix already defined constant

--- functions.php
+++ functions.php
@@ -2,5 +2,7 @@
 /*
  *	Tell DMS we are in a subfolder and fire up the flux capacitors!
 **/
-define( 'DMS_CORE', true );
+if( ! defined( 'DMS_CORE' ) )
+	define( 'DMS_CORE', true );
+
 require_once( 'dms/functions.php' );
