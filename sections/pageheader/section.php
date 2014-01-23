66f09ef6764f73a115e602a52569172a76a9a77f
Andrew Powers <andrew@pagelines.com>
2014-01-22 21:26:29 -0800

    add options for page header

--- sections/pageheader/section.php
+++ sections/pageheader/section.php
@@ -14,6 +14,102 @@ class PLPageHeader extends PageLinesSection {
 	
 	function section_opts(){
 		$options = array();
+		
+	//	$options['config']	= array();
+		$options['config'] = array(
+			'title' => __( 'Header Config', 'pagelines' ),
+			'type'	=> 'multi',
+			'opts'	=> array(
+				
+				array(
+					'key'			=> 'ph_format',
+					'label' 		=> __( 'Format', 'pagelines' ),
+					'type'			=> 'select',
+					'opts'	=> array(
+						'left-side'	=> array('name'=> 'Text On Left'),
+						'centered'		=> array('name'=> 'Centered'),
+					)
+				),
+				array(
+					'key'			=> 'ph_mode',
+					'label' 		=> __( 'Mode', 'pagelines' ),
+					'type'			=> 'select',
+					'opts'	=> array(
+						'nav'	=> array('name'=> 'Use Nav Menu'),
+						'links'	=> array('name'=> 'Use Link Buttons'),
+					)
+				),
+				array(
+					'key'			=> 'ph_padding',
+					'type' 			=> 'text_small',
+					'label' 		=> __( 'Header Top/Bottom Padding in px', 'pagelines' ),
+				),
+			)
+		);
+		$options['content'] = array(
+			'title' => __( 'Header Text', 'pagelines' ),
+			'type'	=> 'multi',
+			'col'	=> 2,
+			'opts'	=> array(
+				array(
+					'key'			=> 'ph_header',
+					'type' 			=> 'text',
+					'label' 		=> __( 'Header Text', 'pagelines' ),
+				),
+				array(
+					'key'			=> 'ph_sub',
+					'type' 			=> 'text',
+					'label' 		=> __( 'Header Sub Text', 'pagelines' ),
+				),
+			)
+		);
+		
+		$options['meta'] = array(
+				'title' => __( 'Header Meta', 'pagelines' ),
+				'type'	=> 'multi',
+				'col'	=> 2,
+				'opts'	=> array(
+					array(
+						'key'			=> 'ph_header',
+						'type' 			=> 'select_menu',
+						'label' 		=> __( 'Header Menu (menu mode only)', 'pagelines' ),
+					),
+					array(
+						'key'			=> 'ph_link1',
+						'type' 			=> 'button_link',
+						'label' 		=> __( 'Header Link 1 (link mode only)', 'pagelines' ),
+					),
+					array(
+						'key'			=> 'ph_link2',
+						'type' 			=> 'button_link',
+						'label' 		=> __( 'Header Link 2 (link mode only)', 'pagelines' ),
+					),
+				)			
+		); 
+		
+		$options['background'] = array(
+			'title' => __( 'Header Background', 'pagelines' ),
+			'type'	=> 'multi',
+			'col'	=> 3,
+			'opts'	=> array(
+				array(
+					'key'			=> 'ph_background',
+					'type' 			=> 'image_upload',
+					'label' 		=> __( 'Header Background Image', 'pagelines' ),
+				),
+				array(
+					'key'			=> 'ph_video',
+					'type' 			=> 'media_select_video',
+					'label' 		=> __( 'Header Link 1 (link mode only)', 'pagelines' ),
+				),
+				array(
+					'key'			=> 'ph_color',
+					'type' 			=> 'color',
+					'label' 		=> __( 'Header Background Color (optional)', 'pagelines' ),
+				),
+			)
+		);
+		
 		return $options;
 
 	}
@@ -29,7 +125,7 @@ class PLPageHeader extends PageLinesSection {
 		
 
 		?>
-		<div class="pl-ph-container <?php echo $format;?>">
+		<div class="pl-ph-container pl-contrast <?php echo $format;?>">
 			<div class="pl-content fix pl-centerer">
 				<div class="ph-text">
 					<h2 class="ph-head" data-sync="icallout_text"><?php echo $title; ?></h2>
