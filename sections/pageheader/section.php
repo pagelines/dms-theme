081b83a3e98120b668ae07e7232e547a51fef49c
Andrew Powers <andrew@pagelines.com>
2014-01-22 21:01:01 -0800

    work on new section

--- sections/pageheader/section.php
+++ sections/pageheader/section.php
@@ -5,7 +5,7 @@
 	Author URI: http://www.pagelines.com
 	Description: A dynamic page header area that supports image background and sub navigation.
 	Class Name: PLPageHeader
-	Filter: component
+	Filter: full-width, component
 	Loading: active
 */
 
@@ -13,85 +13,32 @@ class PLPageHeader extends PageLinesSection {
 
 	
 	function section_opts(){
-		$opts = array(
-			array(
-				'type' 			=> 'select',
-				'title' 		=> 'Select Format',
-				'key'			=> 'icallout_format',
-				'label' 		=> __( 'Callout Format', 'pagelines' ),
-				'opts'=> array(
-					'top'			=> array( 'name' => __( 'Text on top of button', 'pagelines' ) ),
-					'inline'	 	=> array( 'name' => __( 'Text/Button Inline', 'pagelines' ) )
-				),
-			),
-			array(
-				'type' 			=> 'multi',
-				'title' 		=> __( 'Callout Text', 'pagelines' ),
-				'opts'	=> array(
-					array(
-						'key'			=> 'icallout_text',
-						'version' 		=> 'pro',
-						'type' 			=> 'text',
-						'label' 		=> __( 'Callout Text', 'pagelines' ),
-					),
-
-				)
-			),
-			array(
-				'type' 			=> 'multi',
-				'title' 		=> 'Link/Button',
-				'opts'	=> array(
-
-					 array(
-						'key'			=> 'icallout_link',
-						'type' 			=> 'text',
-						'label'			=> __( 'URL', 'pagelines' )
-					),
-					array(
-						'key'			=> 'icallout_target',
-						'type'			=> 'check',
-						'default'		=> false,
-						'label'			=> __( 'Open link in new window', 'pagelines' )
-					),
-					array(
-						'key'			=> 'icallout_link_text',
-						'type' 			=> 'text',
-						'label'			=> __( 'Text on Button', 'pagelines' )
-					),
-					array(
-						'key'			=> 'icallout_btn_theme',
-						'type' 			=> 'select_button',
-						'label'			=> __( 'Button Color', 'pagelines' ),
-					),
-
-				)
-			)
-
-		);
-
-		return $opts;
+		$options = array();
+		return $options;
 
 	}
 
 	function section_template() {
 
-		$text = $this->opt('icallout_text');
-		$format = ( $this->opt('icallout_format') ) ? 'format-'.$this->opt('icallout_format') : 'format-inline';
-		$link = $this->opt('icallout_link');
-		$link_target = ( $this->opt( 'icallout_target', $this->oset ) ) ? ' target="_blank"': '';
-		$theme = ($this->opt('icallout_btn_theme')) ? $this->opt('icallout_btn_theme') : 'btn-primary';
-		$link_text = ( $this->opt('icallout_link_text') ) ? $this->opt('icallout_link_text') : 'Learn More <i class="icon-angle-right"></i>';
-
-		if(!$text && !$link){
-			$text = __("Call to action!", 'pagelines');
-		}
+		$format = '';
+		$title = 'Testing';
+		$text = 'Testing';
+		$style = '';
+		$link = '';
+		$link_text = 'Test';
+		
 
 		?>
-		<div class="icallout-container <?php echo $format;?>">
-
-			<h2 class="icallout-head" data-sync="icallout_text"><?php echo $text; ?></h2>
-			<a class="icallout-action btn <?php echo $theme;?> btn-large" href="<?php echo $link; ?>" <?php if($link_target){ ?> target="_blank" <?php } ?>  data-sync="icallout_link_text"><?php echo $link_text; ?></a>
-
+		<div class="pl-ph-container <?php echo $format;?>">
+			<div class="pl-content fix pl-centerer">
+				<div class="ph-text">
+					<h2 class="ph-head" data-sync="icallout_text"><?php echo $title; ?></h2>
+					<div class="ph-sub"><?php echo $text; ?></div>
+				</div>
+				<div class="ph-meta pl-centered">
+				<a class="icallout-action btn <?php echo $style;?> btn-large" href="<?php echo $link; ?>" data-sync="icallout_link_text"><?php echo $link_text; ?></a>
+				</div>
+			</div>
 		</div>
 	<?php
 
