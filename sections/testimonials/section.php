a2a8dc08fcfc71013277b1852a866fa575ca935c
Andrew Powers <andrew@pagelines.com>
2014-01-28 11:43:07 -0800

    testimonials edits

--- sections/testimonials/section.php
+++ sections/testimonials/section.php
@@ -5,7 +5,7 @@
 	Author URI: http://www.pagelines.com
 	Description: List testimonials with quotes, links and gravatar images.
 	Class Name: PLTestimonials
-	Filter: component
+	Filter: social
 	Loading: active
 */
 
@@ -21,8 +21,45 @@ class PLTestimonials extends PageLinesSection {
 		$options = array();
 		
 		$options[] = array(
+			'key'		=> 'pl_testimonial_config',
+	    	'type'		=> 'multi', 
+			'col'		=> 1,
+			'title'		=> __('Testimonials Config', 'pagelines'), 
+			'opts'	=> array(
+				array(
+					'key'	=> 'testimonials_mode',
+					'label'	=> __( 'Mode', 'pagelines' ),
+					'type'	=> 'select',
+					'opts'	=> array(
+						'default'	=> array('name' => 'Default "dot" navigation'),
+						'avatar'	=> array('name' => 'Use author gravatars'),
+					),
+				),
+			
+				array(
+					'key'	=> 'testimonials_height',
+					'label'	=> __( 'Use consistent height?', 'pagelines' ),
+					'type'	=> 'check'
+				),
+				array(
+					'key'	=> 'testimonials_disable_auto',
+					'label'	=> __( 'Disable Automatically Transition?', 'pagelines' ),
+					'type'	=> 'check'
+				),
+				array(
+					'key'	=> 'testimonials_speed',
+					'label'	=> __( 'Time per quote in ms (ex: 10000, auto transition only) ', 'pagelines' ),
+					'type'	=> 'text_small'
+				),
+				
+
+			)
+	    );
+		
+		$options[] = array(
 			'key'		=> 'pl_testimonial_array',
 	    	'type'		=> 'accordion', 
+			'col'		=> 2,
 			'title'		=> __('Testimonials Setup', 'pagelines'), 
 			'post_type'	=> __('Testimonial', 'pagelines'), 
 			'opts'	=> array(
@@ -34,6 +71,11 @@ class PLTestimonials extends PageLinesSection {
 				array(
 					'key'	=> 'cite',
 					'label'	=> __( 'Citation', 'pagelines' ),
+					'type'	=> 'text'
+				),
+				array(
+					'key'	=> 'email',
+					'label'	=> __( 'Email (Gravatar Mode Only)', 'pagelines' ),
 					'type'	=> 'text'
 				),
 				
@@ -68,25 +110,42 @@ class PLTestimonials extends PageLinesSection {
 
 
 		} 
+		
+		$mode = ( $this->opt('testimonials_mode')) ? $this->opt('testimonials_mode') : 'default';
+		$height = ( $this->opt('testimonials_height')) ? 'true' : 'false';
+		$auto = ( $this->opt('testimonials_disable_auto')) ? 'false' : 'true';
+		$speed = ( $this->opt('testimonials_speed')) ? $this->opt('testimonials_speed') : '10000';
+		
+		
 
 		?>
-		<div class="pl-testimonials-container">
+		<div class="pl-testimonials-container" 
+			data-mode="<?php echo $mode; ?>" 
+			data-height="<?php echo $height; ?>" 
+			data-auto="<?php echo $auto; ?>" 
+			data-speed="<?php echo $speed; ?>" 
+		>
 		  <ul class="pl-testimonials">
 			
 			<?php foreach( $item_array as $item ): 
 				
 					$text = pl_array_get( 'text', $item ); 
 					$cite = pl_array_get( 'cite', $item );
+					$email = pl_array_get( 'email', $item );
 					
 					if( $text == '')
 						continue;
+						
+					$avatar = get_avatar( $email );
+					$avatar_url = pl_get_avatar_url($avatar);
+					$avatar_data = sprintf('data-avatar="%s"', $avatar_url);
 				?>
-		  <li>
-		    <blockquote>
-		    	<p><?php echo $text; ?></p>
-		    	<cite><?php echo $cite; ?></cite>
-		    </blockquote>
-		  </li>
+		  		<li <?php echo $avatar_data;?> >
+				    <blockquote>
+				    	<p><?php echo $text; ?></p>
+				    	<cite><?php echo $cite; ?></cite>
+				    </blockquote>
+				  </li>
 			<?php endforeach; ?>
 		  
 		  </ul>
