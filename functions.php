ef6b7cf0a780fcbe26d346a4237f09fcf7b0a2f5
Simon Prosser <simon@pagelines.com>
2014-01-29 00:54:16 +0000

    fix update and re [build]

--- functions.php
+++ functions.php
@@ -11,4 +11,4 @@ require_once( 'dms/functions.php' );
 
 dms_suggest_plugin( 'Contact Form 7', 'contact-form-7', 'Contact Form 7 can manage multiple contact forms, plus you can customize the form and the mail contents flexibly with simple markup.<br />The form supports Ajax-powered submitting, CAPTCHA, Akismet spam filtering and so on.' );
 
-dms_suggest_plugin( 'WordPress SEO by Yoast', 'wordpress-seo', 'Improve your WordPress SEO: Write better content and have a fully optimized WordPress site using the WordPress SEO plugin by Yoast.' );
+dms_suggest_plugin( 'WordPress SEO', 'wordpress-seo', 'Improve your WordPress SEO: Write better content and have a fully optimized WordPress site using the WordPress SEO plugin by Yoast.' );
