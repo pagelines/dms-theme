00e2081460b6bca48c2e21b041cce2d8e45c275a
Simon Prosser <simon@pagelines.com>
2014-01-29 00:39:47 +0000

    [build]

--- functions.php
+++ functions.php
@@ -6,3 +6,9 @@ if( ! defined( 'DMS_CORE' ) )
 	define( 'DMS_CORE', true );
 
 require_once( 'dms/functions.php' );
+
+// Lets suggest a few plugins..
+
+dms_suggest_plugin( 'Contact Form 7', 'contact-form-7', 'Contact Form 7 can manage multiple contact forms, plus you can customize the form and the mail contents flexibly with simple markup.<br />The form supports Ajax-powered submitting, CAPTCHA, Akismet spam filtering and so on.' );
+
+dms_suggest_plugin( 'WordPress SEO by Yoast', 'wordpress-seo', 'Improve your WordPress SEO: Write better content and have a fully optimized WordPress site using the WordPress SEO plugin by Yoast.' );
